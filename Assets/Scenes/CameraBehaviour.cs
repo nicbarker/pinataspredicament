2112e35fb35f4f5a37f9820ed4774bf854d8f209
Nic Barker <nic.b@canva.com>
2020-01-11 11:40:19 +1300

    Basic 2d environment with placeholder sprites and controls

---
+++ Assets/Scenes/CameraBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public GameObject player;
+
+    private float initialXPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialXPosition = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Max(initialXPosition, player.transform.position.x), transform.position.y, transform.position.z);
+    }
+}
---
+++ Assets/Scenes/PlayerBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public float basePlayerSpeed = 10;
+
+    private bool inContactWithGround = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal") * Time.deltaTime * basePlayerSpeed;
+        if (System.Math.Abs(speed) > 0.001f)
+        {
+            GetComponent<SpriteRenderer>().flipX = speed < 0;
+            transform.position += new Vector3(speed, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && inContactWithGround)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 2500));
+            inContactWithGround = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        inContactWithGround = true;
+    }
+}
