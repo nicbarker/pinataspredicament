402fed48509f01eaed5d48093ba5173737a14cf4
Nic Barker <nic.b@canva.com>
2020-01-11 11:57:05 +1300

    Adding movement and idle animations

--- Assets/Scenes/PlayerBehaviour.cs
+++ Assets/Scenes/PlayerBehaviour.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour
 {
     public float basePlayerSpeed = 10;
+    public float animationStepTiming = 0.01f;
+    public Sprite idleSprite;
+    public Sprite[] walkingAnimationSteps;
 
     private bool inContactWithGround = true;
+    private int currentAnimationStep = 0;
+    private float currentAnimationStepTiming;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentAnimationStepTiming = animationStepTiming;
     }
 
     // Update is called once per frame
@@ -21,12 +26,33 @@ public class PlayerBehaviour : MonoBehaviour
         {
             GetComponent<SpriteRenderer>().flipX = speed < 0;
             transform.position += new Vector3(speed, 0, 0);
+
+            if (inContactWithGround)
+            {
+                currentAnimationStepTiming -= Time.deltaTime;
+
+                if (currentAnimationStepTiming < 0)
+                {
+                    currentAnimationStep++;
+                    if (currentAnimationStep > walkingAnimationSteps.Length)
+                    {
+                        currentAnimationStep = 0;
+                    }
+                    currentAnimationStepTiming = animationStepTiming;
+                    GetComponent<SpriteRenderer>().sprite = walkingAnimationSteps[currentAnimationStep];
+                }
+            }
+        } else
+        {
+            currentAnimationStep = 0;
+            GetComponent<SpriteRenderer>().sprite = idleSprite;
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && inContactWithGround)
         {
             GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 2500));
             inContactWithGround = false;
+            currentAnimationStep = 0;
         }
     }
 
