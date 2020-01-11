d9f8aafafccedb2295ba703dbdc2157445b057c4
Christian Scott <christian.s@canva.com>
2020-01-11 23:20:59 +1100

    refactor throttling

--- Assets/Scenes/DiceGunBehaviour.cs
+++ Assets/Scenes/DiceGunBehaviour.cs
@@ -1,50 +1,10 @@
 ﻿using UnityEngine;
 
-interface DiceGunShotStatus
-{
-    bool HasBeenShot();
-
-    System.TimeSpan TimeSinceLastShot();
-}
-
-class DiceGunHasNotBeenShot : DiceGunShotStatus
-{
-    public bool HasBeenShot()
-    {
-        return false;
-    }
-
-    public System.TimeSpan TimeSinceLastShot()
-    {
-        throw new System.NotImplementedException();
-    }
-}
-
-class DiceGunHasBeenShot : DiceGunShotStatus
-{
-    public DiceGunHasBeenShot(System.DateTime lastShotAt)
-    {
-        this.lastShotAt = lastShotAt;
-    }
-
-    private System.DateTime lastShotAt { get; }
-
-    public bool HasBeenShot()
-    {
-        return true;
-    }
-
-    public System.TimeSpan TimeSinceLastShot()
-    {
-        return System.DateTime.UtcNow.Subtract(lastShotAt);
-    }
-}
-
 public class DiceGunBehaviour : MonoBehaviour
 {
     public GameObject diceGunProjectilePrefab;
 
-    private DiceGunShotStatus shotStatus = new DiceGunHasNotBeenShot();
+    private System.DateTime? lastShotAt = null;
 
     public bool TryShoot(Vector3 startPosition, bool isFacingLeft)
     {
@@ -53,8 +13,15 @@ public class DiceGunBehaviour : MonoBehaviour
             return false;
         }
 
-        shotStatus = new DiceGunHasBeenShot(lastShotAt: System.DateTime.UtcNow);
+        lastShotAt = System.DateTime.UtcNow;
 
+        Shoot(startPosition, isFacingLeft);
+
+        return true;
+    }
+
+    private void Shoot(Vector3 startPosition, bool isFacingLeft)
+    {
         GameObject projectile = Instantiate(diceGunProjectilePrefab);
         projectile.transform.position = startPosition;
 
@@ -62,18 +29,15 @@ public class DiceGunBehaviour : MonoBehaviour
         projectile
             .GetComponent<Rigidbody2D>()
             .AddForce(new Vector2(xAxisForceComponent, 0));
-
-        return true;
     }
 
-    private bool CanShoot()
-    {
-        if (shotStatus.HasBeenShot())
+    private bool CanShoot() =>
+        lastShotAt switch
         {
-            var timeSinceLastShot = shotStatus.TimeSinceLastShot();
-            return timeSinceLastShot.TotalMilliseconds > 500;
-        }
+            null => true, // has not been shot yet
+            System.DateTime lastShotAt => MillisecondsSince(lastShotAt) > 500,
+        };
 
-        return true;
-    }
+    private double MillisecondsSince(System.DateTime time) =>
+        System.DateTime.UtcNow.Subtract(time).TotalMilliseconds;
 }
