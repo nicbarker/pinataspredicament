6517faeb36e66be6ffea9649d781b51ce32b1492
Christian Scott <christian.s@canva.com>
2020-01-11 15:45:34 +1100

    remove ability store

--- Assets/Scenes/GameData.cs
+++ Assets/Scenes/GameData.cs
@@ -3,42 +3,13 @@ using UnityEngine;
 
 public class GameData : MonoBehaviour
 {
-    private class AbilityStore
-    {
-        private readonly Dictionary<Ability, float> timers = new Dictionary<Ability, float>()
-        {
-            { Ability.MOVE_LEFT, 0f },
-            { Ability.MOVE_RIGHT, 0f },
-            { Ability.JUMP, 0f }
-        };
-
-        public bool IsActive(Ability ability)
-        {
-            return timers[ability] > 0;
-        }
-
-        public void Activate(Ability ability)
-        {
-            timers[ability] = 30f;
-        }
-
-        public float RemainingTimeFor(Ability ability)
-        {
-            return timers[ability];
-        }
-
-        public void Tick()
-        {
-            var keys = new List<Ability>(timers.Keys);
-            foreach (var key in keys)
-            {
-                timers[key] = Mathf.Max(0, timers[key] - Time.deltaTime);
-            }
-        }
-    }
-
     public int gems = 0;
-    private AbilityStore abilityStore = new AbilityStore();
+    private readonly Dictionary<Ability, float> timers = new Dictionary<Ability, float>()
+    {
+        { Ability.MOVE_LEFT, 30f },
+        { Ability.MOVE_RIGHT, 30f },
+        { Ability.JUMP, 30f }
+    };
 
     // Start is called before the first frame update
     void Start()
@@ -49,14 +20,18 @@ public class GameData : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        abilityStore.Tick();
+        var keys = new List<Ability>(timers.Keys);
+        foreach (var key in keys)
+        {
+            timers[key] = Mathf.Max(0, timers[key] - Time.deltaTime);
+        }
     }
 
     public bool TryActivate(Ability ability)
     {
         if (gems > 0)
         {
-            abilityStore.Activate(ability);
+            timers[ability] = 30f;
             gems--;
             return true;
         }
@@ -66,11 +41,11 @@ public class GameData : MonoBehaviour
 
     public float RemainingTimeFor(Ability ability)
     {
-        return abilityStore.RemainingTimeFor(ability);
+        return timers[ability];
     }
 
     public bool IsAbilityActive(Ability ability)
     {
-        return abilityStore.IsActive(ability);
+        return timers[ability] > 0;
     }
 }
