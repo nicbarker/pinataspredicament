6efd89c598f503e5e81f56e7e16380f509f9cf80
Nic Barker <nic.b@canva.com>
2020-01-11 18:48:01 +1300

    Added some background assets

---
+++ Assets/Scenes/ScrollingBackgroundBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackgroundBehaviour : MonoBehaviour
+{
+    public GameObject player;
+    public float fractionOfPlayerPosition = 1f;
+
+    private float initialXPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialXPosition = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(
+            Mathf.Max(initialXPosition + player.transform.position.x * fractionOfPlayerPosition, initialXPosition),
+            transform.position.y,
+            transform.position.z
+        );
+    }
+}
