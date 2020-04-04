0a481f4b0efd0e3b2568128ce133e0aebedf7eb1
Nic Barker <nic.b@canva.com>
2020-04-04 18:25:31 +1300

    Redesign the ability activation UI

--- Assets/Scripts/ActivateAbilityBehaviour.cs
+++ Assets/Scripts/ActivateAbilityBehaviour.cs
@@ -1,32 +1,147 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
 using UnityEngine.UI;
+using System;
+
+public enum AbilityState
+{
+  ENTERING,
+  ACTIVE,
+  EXITING,
+  EXITED,
+}
+
+public class AbilityButton
+{
+  public Ability ability;
+  public AbilityState abilityState = AbilityState.EXITED;
+  public GameObject button;
+
+  public float tweenState;
+  public float previousXPosition;
+}
 
 public class ActivateAbilityBehaviour : MonoBehaviour
 {
-    private GameData gameData;
-    public Ability ability;
-    public Image overlay;
+  private GameData gameData;
+  private AbilityButton[] activeAbilities = new AbilityButton[Enum.GetNames(typeof(Ability)).Length];
+  private int activeAbilitiesCount;
+  public GameObject UIButtonPrefab;
+  static int TOP_OFFSET = 500;
+  static int BUTTON_GUTTER = 16;
+  static float TWEEN_INCREMENT = 0.05f;
+  static float BUTTON_SCALE_FACTOR = 1.7f;
 
-    private Button button;
 
-    void Start()
+  void Start()
+  {
+    this.gameData = GameObject.FindGameObjectWithTag("GameData").GetComponent<GameData>();
+    int i = 0;
+    foreach (KeyValuePair<Ability, float> entry in gameData.abilityTimers)
     {
-        this.gameData = GameObject.FindGameObjectWithTag("GameData").GetComponent<GameData>();
-        button = GetComponent<Button>();
-        button.onClick.AddListener(() => gameData.TryActivate(ability));
+      AbilityButton abilityButton = new AbilityButton();
+      abilityButton.button = Instantiate(UIButtonPrefab, this.gameObject.transform);
+      var abilityImage = abilityButton.button.GetComponent<Image>();
+      abilityImage.color = new Color(255, 255, 255, 0);
+      abilityImage.sprite = (Sprite)AssetDatabase.LoadAssetAtPath(AbilityExtensions.GetImagePath(entry.Key), typeof(Sprite));
+      abilityImage.transform.localScale = new Vector3(0f, 0f, 0f);
+      abilityButton.button.GetComponent<RectTransform>().localPosition = new Vector3(0, TOP_OFFSET, 0);
+
+      abilityButton.abilityState = AbilityState.EXITED;
+      abilityButton.ability = entry.Key;
+      activeAbilities[i] = abilityButton;
+      i++;
     }
+  }
 
-    void Update()
+  void Update()
+  {
+    int activeIndex = 0;
+    Array.Sort(this.activeAbilities, delegate (AbilityButton a, AbilityButton b) { return gameData.abilityTimers[a.ability] > gameData.abilityTimers[b.ability] ? 1 : -1; });
+    for (int i = 0; i < this.activeAbilities.Length; i++)
     {
-        if (gameData.IsAbilityActive(ability))
+      var activeAbility = this.activeAbilities[i];
+      // Set to EXITING when ability has expired
+      if (activeAbility.abilityState == AbilityState.ACTIVE && gameData.abilityTimers[activeAbility.ability] == 0)
+      {
+        activeAbility.abilityState = AbilityState.EXITING;
+        activeAbilitiesCount--;
+        activeAbility.tweenState = 0;
+      }
+      // Set to ENTERING when ability is first activated
+      else if (activeAbility.abilityState == AbilityState.EXITED && gameData.abilityTimers[activeAbility.ability] > 0)
+      {
+        activeAbility.abilityState = AbilityState.ENTERING;
+        activeAbility.tweenState = 0;
+        activeAbilitiesCount++;
+      }
+
+      // Update tween positions of entering and active abilities
+      if (activeAbility.abilityState == AbilityState.ACTIVE)
+      {
+        RectTransform transform = activeAbility.button.GetComponent<RectTransform>();
+        float localWidth = (transform.rect.width * BUTTON_SCALE_FACTOR);
+        float newPosition = (activeIndex * (localWidth + BUTTON_GUTTER)) -
+          (
+            (activeAbilitiesCount - 1) *
+            ((localWidth + BUTTON_GUTTER) * 0.5f)
+          );
+
+        if (newPosition != transform.localPosition.x)
         {
-            button.interactable = true;
-            overlay.fillAmount = gameData.RemainingFractionFor(ability);
+          transform.localPosition = new Vector3(Mathf.Lerp(activeAbility.previousXPosition, newPosition, activeAbility.tweenState), TOP_OFFSET, 0);
+          activeAbility.tweenState += TWEEN_INCREMENT;
         }
         else
         {
-            button.interactable = gameData.CanActivate(ability);
-            overlay.fillAmount = 0f;
+          activeAbility.previousXPosition = newPosition;
+          activeAbility.tweenState = 0;
         }
+        activeAbility.button.GetComponentsInChildren<Image>()[1].fillAmount = gameData.RemainingFractionFor(activeAbility.ability);
+        activeIndex++;
+      }
+      else if (activeAbility.abilityState == AbilityState.ENTERING)
+      {
+        RectTransform transform = activeAbility.button.GetComponent<RectTransform>();
+        float localWidth = (transform.rect.width * BUTTON_SCALE_FACTOR);
+        float newPosition = (activeIndex * (localWidth + BUTTON_GUTTER)) -
+          (
+            (activeAbilitiesCount - 1) *
+            ((localWidth + BUTTON_GUTTER) * 0.5f)
+          );
+        transform.localPosition = new Vector3(newPosition, TOP_OFFSET, 0);
+
+        if (activeAbility.tweenState < 1)
+        {
+          activeAbility.button.GetComponent<Image>().color = new Color(255, 255, 255, activeAbility.tweenState);
+          activeAbility.button.transform.localScale = new Vector3(BUTTON_SCALE_FACTOR * activeAbility.tweenState, BUTTON_SCALE_FACTOR * activeAbility.tweenState, BUTTON_SCALE_FACTOR * activeAbility.tweenState);
+          activeAbility.tweenState += TWEEN_INCREMENT;
+        }
+        else
+        {
+          activeAbility.tweenState = 0;
+          activeAbility.abilityState = AbilityState.ACTIVE;
+        }
+        activeAbility.button.GetComponentsInChildren<Image>()[1].fillAmount = gameData.RemainingFractionFor(activeAbility.ability);
+        activeIndex++;
+      }
+      else if (activeAbility.abilityState == AbilityState.EXITING)
+      {
+        if (activeAbility.tweenState < 1)
+        {
+          var invertedTweenState = 1 - activeAbility.tweenState;
+          activeAbility.button.GetComponent<Image>().color = new Color(255, 255, 255, invertedTweenState);
+          activeAbility.button.transform.localScale = new Vector3(BUTTON_SCALE_FACTOR * invertedTweenState, BUTTON_SCALE_FACTOR * invertedTweenState, BUTTON_SCALE_FACTOR * invertedTweenState);
+          activeAbility.tweenState += TWEEN_INCREMENT;
+        }
+        else
+        {
+          activeAbility.tweenState = 0;
+          activeAbility.abilityState = AbilityState.EXITED;
+        }
+      }
     }
+
+  }
 }
