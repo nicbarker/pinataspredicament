2bd9e45079d0de12a26afa2ec7d5f53586c32cfa
Nic Barker <nic.b@canva.com>
2020-04-17 15:25:24 +1200

    Modify camera tracking behaviour, continue to implement level 2

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -16,9 +16,17 @@ public class CameraBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var cameraPositionY = transform.position.y;
+        var topScreenBoundary = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height * 0.6f, 0)).y;
+        var bottomScreenBoundary = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height * 0.3f, 0)).y;
+        if (player.transform.position.y > topScreenBoundary) {
+            cameraPositionY += player.transform.position.y - topScreenBoundary + 0.01f;
+        } else if (player.transform.position.y < bottomScreenBoundary) {
+            cameraPositionY += player.transform.position.y - bottomScreenBoundary - 0.01f;
+        }
         transform.position = new Vector3(
             Mathf.Max(initialXPosition, player.transform.position.x),
-            Mathf.Max(initialYPosition, player.transform.position.y),
+            cameraPositionY,
             transform.position.z
         );
 
